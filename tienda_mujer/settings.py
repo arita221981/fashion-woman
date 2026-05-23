b2f0275acb90d288d5e1777612379ce9446ecbb2
Ursula Alvarado <ursula172722@gmail.com>
2026-05-22 22:26:59 -0600

    Fix: Configurar WhiteNoise para estilos CSS

--- tienda_mujer/settings.py
+++ tienda_mujer/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -106,3 +107,4 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 CSRF_TRUSTED_ORIGINS = ['https://fashion-woman-production.up.railway.app']
+STATIC_ROOT = BASE_DIR / "staticfiles"
